815e0e6d07736f04c722cb76e765a8e08c6a0225
SophieC <sophie.cooperman@taboola.com>
2020-10-05 22:35:49 +0300

    case fix

--- src/components/board.js
+++ src/components/board.js
@@ -11,7 +11,7 @@ const Board = () => {
     const [currentPlayer, setCurrentPlayer] = useState(true);
     const [counter, setCounter] = useState(0);
 
-    let symbol = { true: "X", false: "O" };
+    const symbol = { true: "X", false: "O" };
     const WON_THE_GAME_TXT = `Game Over! Player ${symbol[!currentPlayer]} won!`;
     const NO_WIN = "Game Over - but no winner";
 
@@ -22,12 +22,11 @@ const Board = () => {
 
 
     const resetBoard = () => {
-        setBoardGrid(initialBoard);
+        setBoardGrid([...initialBoard]);
         setWinTitle(EMPTY_STRING);
         setCurrentPlayer(true);
         setCounter(0);
     }
-
 
     const onClick = (rowIdx, butonIdx) => {
         if (WinTitle === EMPTY_STRING && boardGrid[rowIdx][butonIdx] === "") {
@@ -35,11 +34,10 @@ const Board = () => {
             setBoardGrid(boardGrid);
             setCurrentPlayer(!currentPlayer);
             setCounter(counter + 1);
-            console.log("counter: " + counter);
         }
     }
 
-    const Square = ({ rowIdx, butonIdx, buttonSymbol }) => {
+    const Cell = ({ rowIdx, butonIdx, buttonSymbol }) => {
         return (
             <button className="buttonStyle" onClick={() => onClick(rowIdx, butonIdx)}>
                 {`${buttonSymbol}`}
@@ -50,53 +48,53 @@ const Board = () => {
     const detectWin = () => {
         let columnsX = [0, 0, 0];
         let columnsO = [0, 0, 0];
-        boardGrid.map(row => {
-            debugger;
+
+        boardGrid.some(row => {
             // row check
             if (row[0] === row[1] && row[1] === row[2] && row[0] !== EMPTY_STRING) {
                 setWinTitle(WON_THE_GAME_TXT);
-                return row;
+                return true;
             }
 
             // column check
-            row.map((item, index) => {
-                if (item === symbol[true]) {
+            row.some((cell, index) => {
+                if (cell === symbol[true]) {
                     columnsX[index]++;
                 }
-                else if (item === symbol[false]) {
+                else if (cell === symbol[false]) {
                     columnsO[index]++;
                 }
-                return item;
             });
 
             if (columnsX.includes(3) || columnsO.includes(3)) {
-                debugger;
                 setWinTitle(WON_THE_GAME_TXT);
-                return row;
+                return true;
             }
-            return row;
         });
 
-        // diagonals
-        if ((boardGrid[0][0] === boardGrid[1][1] && boardGrid[1][1] === boardGrid[2][2] && boardGrid[0][0] !== EMPTY_STRING) ||
-            (boardGrid[0][2] === boardGrid[1][1] && boardGrid[1][1] === boardGrid[2][0] && boardGrid[0][2] !== EMPTY_STRING)) {
+        // diagonals check
+        const diagonal1 = boardGrid[0][0] === boardGrid[1][1] && boardGrid[1][1] === boardGrid[2][2] && boardGrid[0][0] !== EMPTY_STRING;
+        const diagonal2 = boardGrid[0][2] === boardGrid[1][1] && boardGrid[1][1] === boardGrid[2][0] && boardGrid[0][2] !== EMPTY_STRING;
+        if (WinTitle === EMPTY_STRING && (diagonal1 || diagonal2)) {
             setWinTitle(WON_THE_GAME_TXT);
+            return;
         }
 
         // No wins
-        if (counter === 9) {
+        if (WinTitle === EMPTY_STRING && counter === 9) {
             setWinTitle(NO_WIN);
             return;
         }
+
     }
 
     const renderBoard = () => {
         const items = boardGrid.map((row, index) => {
             return (
                 <tr key={index}>
-                    <td><Square rowIdx={index} butonIdx={0} buttonSymbol={row[0]} /></td>
-                    <td><Square rowIdx={index} butonIdx={1} buttonSymbol={row[1]} /></td>
-                    <td><Square rowIdx={index} butonIdx={2} buttonSymbol={row[2]} /></td>
+                    <td><Cell rowIdx={index} butonIdx={0} buttonSymbol={row[0]} /></td>
+                    <td><Cell rowIdx={index} butonIdx={1} buttonSymbol={row[1]} /></td>
+                    <td><Cell rowIdx={index} butonIdx={2} buttonSymbol={row[2]} /></td>
                 </tr>
             )
         });
@@ -107,13 +105,14 @@ const Board = () => {
     return (
         <div className="boardContainer">
             { WinTitle === EMPTY_STRING ?
-            <div>
-                <h1> Welcome to tic tac toe game! </h1>
-                <h2> {`Turn: ${symbol[currentPlayer]}`}</h2>
-            </div>
-            : <h1>{WinTitle}</h1>}
+                <div>
+                    <h1> Welcome to tic tac toe game! </h1>
+                    <h2> {`Turn: ${symbol[currentPlayer]}`}</h2>
+                </div>
+                : <h1>{WinTitle}</h1>}
 
             {renderBoard()}
+            
             {WinTitle !== EMPTY_STRING ? <button className="newGameBtn" onClick={resetBoard}>New Game</button> : null}
         </div>
     )
